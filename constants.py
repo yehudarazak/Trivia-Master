5ae729b88d64e74300876df6fdf80cf884e7e323
yehudarazak <146989086+yehudarazak@users.noreply.github.com>
2024-12-15 17:04:05 +0200

    initial commit

---
+++ constants.py
@@ -0,0 +1,24 @@
+RAW_QUESTIONS = [
+  {"question": "Can ostriches fly?", "answer": "no"},
+  {"question": "Do goldfish have a memory span of only 3 seconds?", "answer": "no"},
+  {"question": "Is the Great Wall of China visible from space with the naked eye?", "answer": "no"},
+  {"question": "Is the Earth perfectly round?", "answer": "no"},
+  {"question": "Was Albert Einstein awarded the Nobel Prize for his theory of relativity?", "answer": "no"},
+  {"question": "Is Australia wider than the moon?", "answer": "yes"},
+  {"question": "Is lightning hotter than the surface of the sun?", "answer": "yes"},
+  {"question": "Can humans breathe and swallow at the same time?", "answer": "no"},
+  {"question": "Do penguins live at the North Pole?", "answer": "no"},
+  {"question": "Is chocolate toxic to dogs?", "answer": "yes"},
+  {"question": "Is Mount Everest the tallest mountain in the world?", "answer": "yes"},
+  {"question": "Do sharks have bones?", "answer": "no"},
+  {"question": "Is a tomato a fruit?", "answer": "yes"},
+  {"question": "Do all birds lay eggs?", "answer": "yes"},
+  {"question": "Is the speed of sound faster than the speed of light?", "answer": "no"},
+  {"question": "Did the Titanic sink in 1912?", "answer": "yes"},
+  {"question": "Can honey spoil over time?", "answer": "no"},
+  {"question": "Is the capital of Australia Sydney?", "answer": "no"},
+  {"question": "Are polar bears left-handed?", "answer": "no"},
+  {"question": "Does an octopus have three hearts?", "answer": "yes"},
+]
+
+QUESTIONS = RAW_QUESTIONS.copy()
---
+++ main.py
@@ -0,0 +1,90 @@
+import random
+import customtkinter as ctk
+from constants import *
+
+bin_of_questions = []
+
+score = 0
+def new_question():
+    global question_and_answer
+    question_and_answer = random.choice(QUESTIONS)
+    return question_and_answer
+
+def delete_question(question_with_answer):
+    QUESTIONS.remove(question_with_answer)
+
+
+
+
+
+def show_qustions():
+    global question_and_answer
+    if len(QUESTIONS) > 0:
+        question = new_question()["question"]
+        question_label.configure(text=question)
+    else:
+        end_game()
+
+
+
+def check_answer(user_answer):
+    global score, question_and_answer
+    answer = question_and_answer["answer"]
+
+    if user_answer == answer:
+        score += 1
+
+    bin_of_questions.append(question_and_answer)
+    delete_question(question_and_answer)
+    show_qustions()
+    
+    
+def answer_yes():
+    check_answer("yes")
+
+
+def answer_no():
+     check_answer("no")
+
+def play_again():
+    global score, bin_of_questions, QUESTIONS
+    score = 0
+    QUESTIONS = bin_of_questions
+    play_again_button.pack_forget()
+    yes_button.pack(side="right", padx=50, pady=10)
+    no_button.pack(side="left", padx=50, pady=10)
+    show_qustions()
+
+def end_game():
+    global score
+    question_label.configure(text= f"Game Over!\n Your score: {score}/{len(bin_of_questions)}")
+    yes_button.pack_forget()
+    no_button.pack_forget()
+    play_again_button.pack(pady=20)
+
+
+game_screen = ctk.CTk()
+game_screen.title("Trivia Master")
+game_screen.geometry("450x250")
+
+game_title_lable = ctk.CTkLabel(game_screen, text="Trivia Master", font=("monospaced", 30))
+game_title_lable.pack(pady= 20)
+
+
+question_label = ctk.CTkLabel(game_screen, text="", font=("monospaced", 18), wraplength=350)
+question_label.pack(pady=20)
+
+
+
+yes_button = ctk.CTkButton(game_screen, text="yes", hover=True, command=answer_yes )
+yes_button.pack(side="right", padx=50, pady=10)
+
+no_button = ctk.CTkButton(game_screen, text="no", hover=True, command=answer_no )
+no_button.pack(side="left", padx=50, pady=10)
+
+play_again_button = ctk.CTkButton(game_screen, text="Play Again", command=play_again)
+play_again_button.pack()
+play_again_button.pack_forget()
+
+show_qustions()
+game_screen.mainloop()
